3eeccbc20504d984b61b09760541ab43c4e7f52e
NauraNabilah <naurasitinabilah@smkwikrama.sch.id>
2024-09-18 14:26:34 +0700

    update

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,5 +12,6 @@ class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(SignUpForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
